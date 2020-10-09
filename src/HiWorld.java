31f179e217c0d18ee9a04639ab8d6e921fc03345
L-zhen <2216296910@qq.com>
2020-10-09 23:08:30 +0800

    test

---
+++ src/HiWorld.java
@@ -0,0 +1,10 @@
+/**
+ * @Author lz
+ * @Date 2020/10/9 23:04
+ * @Version 1.0
+ */
+public class HiWorld {
+    public static void main(String[] args) {
+        System.out.println("Hi World!");
+    }
+}
